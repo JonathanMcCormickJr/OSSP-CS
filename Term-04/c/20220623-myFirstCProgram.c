f66649b26fcf1b5709f7fa6962511f8dbd7a3274
Jonathan A. McCormick, Jr <67705789+LiberlandHacker@users.noreply.github.com>
2022-06-24 17:07:35 -0400

    Update 20220623-myFirstCProgram.c

--- Term-04/c/20220623-myFirstCProgram.c
+++ Term-04/c/20220623-myFirstCProgram.c
@@ -1,1 +1,12 @@
 
+#include <stdio.h>
+
+int main() {
+    // Prints the size in bytes of each data type
+    printf("int: %ld \n", sizeof(int));
+    printf("float: %ld \n", sizeof(float));
+    printf("double: %ld \n", sizeof(double));
+    printf("char: %ld \n", sizeof(char));
+
+    return 0;
+}
