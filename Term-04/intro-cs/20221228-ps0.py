0b45d0b2dfbf46c45834193220eeb2429a76aa80
Jonathan A. McCormick, Jr <67705789+LiberlandHacker@users.noreply.github.com>
2022-12-29 00:10:02 -0600

    Update 20221228-ps0.py

--- Term-04/intro-cs/20221228-ps0.py
+++ Term-04/intro-cs/20221228-ps0.py
@@ -1,1 +1,32 @@
+"""
+Write a program that does the following in order:
+1. Asks the user to enter a number “x”
+2. Asks the user to enter a number “y” 
+3. Prints out number “x”, raised to the power “y”.
+4. Prints out the log (base 2) of “x”. 
+"""
+
+old_message = """
+Enter number x: 2
+Enter number y: 3
+X**y = 8
+log(x) = 1
+"""
+
+import math
+
+x = int(input("Enter number x: "))
+y = int(input("Enter number y: "))
+print("X**y = " + str(x**y))
+print("log(x) = " + str(int(math.log(x,2))))
+
+new_message = """
+Enter number x: 2
+Enter number y: 3
+X**y = 8
+log(x) = 1
+"""
+
+# This is just to double check that the values that I get from my code match the example exactly. 
+#print(old_message == new_message)
 
