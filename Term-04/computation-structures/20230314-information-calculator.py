95fdd2de92a9927bd22b9f39792aff3e6b167cdb
Jonathan A. McCormick, Jr <67705789+LiberlandHacker@users.noreply.github.com>
2023-03-14 15:32:24 -0500

    Update 20230314-information-calculator.py

--- Term-04/computation-structures/20230314-information-calculator.py
+++ Term-04/computation-structures/20230314-information-calculator.py
@@ -10,8 +10,15 @@ def information_p(p):
 def information_nm(n, m):
     # This is for doing the math with n and m values instead
     return log2(n/m)
-    
-n = float(input("n:"))
-m = float(input("m:"))
 
-print(information_nm(n,m))
+use_p_value = True
+
+if use_p_value == True:
+    p = float(input("p:"))
+    print(information_p(p))
+elif use_p_value == False:
+    n = float(input("n:"))
+    m = float(input("m:"))
+    print(information_nm(n,m))
+else:
+    print("Error: unexpected value.")
